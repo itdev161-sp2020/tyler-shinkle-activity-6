5eda184b8c20e08813bd1286cf262c0a01a2aa45
tylerShinkle <shinklt1@gmatc.matc.edu>
2020-03-29 21:26:48 -0500

    starter code set and first prop passed!

--- a6/my-app/src/index.js
+++ a6/my-app/src/index.js
@@ -1,3 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+//pass props from parente -> children
+
+class Square extends React.Component {
+    render() {
+      return (
+        //render a button with the value of each squares
+        //value prop, <Square value={i}
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      //set the value prop to whatever 'i' value is passed
+      //this is a way we can pass props from our Board component 
+      //to our Square component
+      return <Square value={i}/>;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
