35d1ca9d3a89e4c9249736b217d6f654d78e0763
tylerShinkle <shinklt1@gmatc.matc.edu>
2020-03-30 00:11:59 -0500

    pre time travel game finished!

--- a6/my-app/src/index.js
+++ a6/my-app/src/index.js
@@ -28,24 +28,36 @@ import './index.css';
     handleClick(i){
         //create copy of the squares array with slice()
         const squares = this.state.squares.slice();
-        squares[i]=this.state.xIsNext?'X':'O';
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
         this.setState({
-            //flip xIsNext
-            squares:squares,
-            xIsNext:!this.state.xIsNext
-        });
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
     renderSquare(i) {
       //set the value prop to whatever 'i' value is passed
       //this is a way we can pass props from our Board component 
       //to our Square component
-      return <Square 
+      return (
+            <Square 
                 value={this.state.squares[i]}
-                onClick={()=>this.handleClick(i)}/>
+                onClick={()=>this.handleClick(i)}
+            />
+      );
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+          status = "Winner: " +winner;
+      }else{
+          status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -92,4 +104,24 @@ import './index.css';
     <Game />,
     document.getElementById('root')
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
